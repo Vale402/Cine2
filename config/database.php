f7c95392ca182b0f31bee42fc0706f8513c77e7d
ValeriaPonce <valeriaponce402@gmail.com>
2026-05-20 23:00:58 -0600

    Maestro-esclavo

--- config/database.php
+++ config/database.php
@@ -44,25 +44,30 @@ return [
             'transaction_mode' => 'DEFERRED',
         ],
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+'mysql' => [
+    'read' => [
+        'host' => [
+            //'25.38.183.140', 
+            '127.0.0.1',  // Esclavo (temporalmente maestro mientras compañera no está)
         ],
+    ],
+    'write' => [
+        'host' => [
+            '25.0.239.52',    // Maestro (tu laptop)
+        ],
+    ],
+    'sticky'     => true,
+    'driver'     => 'mysql',
+    'port'       => env('DB_PORT', '3306'),
+    'database'   => env('DB_DATABASE', 'cine_laravel'),
+    'username'   => env('DB_USERNAME', 'laravel'),
+    'password'   => env('DB_PASSWORD', 'laravel123'),
+    'charset'    => 'utf8mb4',
+    'collation'  => 'utf8mb4_unicode_ci',
+    'prefix'     => '',
+    'strict'     => true,
+    'engine'     => null,
+],
 
         'mariadb' => [
             'driver' => 'mariadb',
